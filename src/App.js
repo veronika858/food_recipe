6b6c92c4b3dcabb9b44a047c14a5f4e32da5561f
veronika858 <60067390+veronika858@users.noreply.github.com>
2020-07-23 17:48:54 +0200

    clean

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import "./scss/main.css";
+import Recipe from "./components/Recipe";
+import Alert from "./components/Alert";
+import { v4 as uuidv4 } from "uuid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+	const[query, setQuery] = useState("");
+	const[recipes, setRecipes] = useState([]);
+	const[alert, setAlert] = useState("");
+
+	const YOUR_APP_ID = "3e79a8e1";
+	const YOUR_APP_KEY = "2bb832e46be63e2c97eb80c6405cfd02";
+	const url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
+
+	const getData = async () => {
+		if (query !== "") {
+			const result = await fetch(url);
+			const rs = await result.json();
+
+			if(!rs.more){
+				return setAlert("No food with such name");
+			}
+			console.log(rs);
+			setRecipes(rs.hits);
+			setAlert("");
+			setQuery("");
+		} else {
+			setAlert("Please fill the form")
+		}
+	};
+	const onSubmit = (event) => {
+		event.preventDefault();
+		getData();
+	};
+	const onChange = (event) =>{
+		setQuery(event.target.value);
+	};
+	return (
+  	<div className="app container">
+	    <h1>Food Recipes Searching</h1>
+	    <form className="search-form" onSubmit={onSubmit}>
+		    {alert !== "" && <Alert alert = {alert}/>}
+		    <input className="search" type="text"
+		           placeholder="Search Food"
+		           autoComplete="on"
+		           onChange={onChange}
+		            value={query }/>
+		    <input className="search-submit" type="submit" value="Search"/>
+	    </form>
+	    <div className="recipes">
+		    {recipes !== [] && recipes.map(recipe =>  <Recipe key={uuidv4()} recipe = {recipe}/>)}
+	    </div>
     </div>
   );
-}
+
+};
 
 export default App;
---
+++ src/components/Alert.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Alert = ({alert}) => {
+	return (
+		<div className="alert">
+			<h5>{alert}</h5>
+		</div>
+	);
+};
+export default Alert;
